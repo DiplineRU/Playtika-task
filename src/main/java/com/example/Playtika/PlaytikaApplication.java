b5e59b10deefb866faaa78c20adf8603562a02d3
DiplineRU <artgul52@gmail.com>
2024-10-13 13:32:01 +0300

    initial commit

---
+++ src/main/java/com/example/Playtika/PlaytikaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Playtika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaytikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaytikaApplication.class, args);
+	}
+
+}
